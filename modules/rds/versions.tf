d0ca67d62aed9ac3b856665b4a00e7390c4c40e3
Sandeep Konda <kondasandeep56@gmail.com>
2026-06-08 14:50:42 +0530

    Downgrade postgresql provider version to 1.26.0

--- modules/rds/versions.tf
+++ modules/rds/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     postgresql = {
       source  = "cyrilgdn/postgresql"
-      version = "1.27.0"
+      version = "1.26.0"
     }
     aws = {
       source = "hashicorp/aws"
